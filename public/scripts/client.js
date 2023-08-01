57046963872e2ba80b4d67ec90407b580786c734
Haijoon Kim <abigail.haijoon.kim@gmail.com>
2023-07-31 22:53:11 -0700

    fetching tweets with Ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,41 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Lobster",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@lobsterprince" },
-    "content": {
-      "text": "I am a lobster"
-    },
-    "created_at": 1461113959088
-  }
-];
-
 const renderTweets = function(tweets) {
   for (const tweet of tweets) {
     let tweetElement = createTweetElement(tweet);
@@ -62,7 +27,7 @@ const createTweetElement = function(tweet) {
                   </div>
                   <footer class="tweet-footer">
                     <div class="tweet-footer-date">
-                      ${tweet.created_at}
+                      ${timeago.format(Number(tweet.created_at))}
                     </div>
                     <aside class="tweet-footer-icons">
                       <i class="tweet-footer-icon fa-solid fa-flag fa-xs"></i>
@@ -75,16 +40,30 @@ const createTweetElement = function(tweet) {
 };
 
 $("document").ready(() => {
-  renderTweets(data);
-});
+  const loadTweets = function() {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET',
+      dataType: 'json',
+      success: function(response) {
+        renderTweets(response);
+      },
+      error: function(error) {
+        console.log('Error: ', error);
+      }
+    });
+  };
 
-$("document").ready(() => {
+  loadTweets();
+
+  // Event listner for the tweet form submission
   $('.tweet-text-form').on('submit', function(event) {
     event.preventDefault();
     const formData = $(this).serialize();
 
     console.log('on submit: ', formData);
 
+    // AJAX request to post a new tweet
     $.ajax({
       url: '/tweets',
       method: 'POST',
